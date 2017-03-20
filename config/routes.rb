d40ea8ea1b3a433426278acd049ea3abf05c86cb
wbanguna ybat <jon.david.long@gmail.com>
2017-03-20 09:45:28 +0000

    Add user sign up to UI

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
