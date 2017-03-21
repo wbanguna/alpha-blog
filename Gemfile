4072068c535b208fa21441c3eef0b8e3a4d104ef
wbanguna ybat <jon.david.long@gmail.com>
2017-03-21 11:26:49 +0000

    add pagination gems, and pagination to UserController::show&index and ArticleController::index and respective Views

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
+# pagination
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Secure Password
 gem 'bcrypt', '~> 3.1.7'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -33,7 +33,8 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   
