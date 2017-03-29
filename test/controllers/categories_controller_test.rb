1981340b6388715f671ccc7699a64f7d0ab0f915
wbanguna ybat <jon.david.long@gmail.com>
2017-03-29 10:44:42 +0000

    Admin user requirements category tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "jon", email: "jon@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success    
   end
@@ -21,4 +23,10 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
   
+  test "should redirect create when not admin" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
+  end
 end
